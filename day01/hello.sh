5884b273570c4774e512807a877b57c26a678886
Ubuntu <ubuntu@ip-172-31-42-249.eu-north-1.compute.internal>
2025-05-02 15:31:57 +0000

    added a new file hello.sh

---
+++ day01/hello.sh
@@ -0,0 +1,8 @@
+#i/bin/bash
+
+# this is my blog script 
+
+echo "vijesh:hello guys, please subscribe my hashnode blog channel"
+echo "learners:ok bhaiya , hum subscribe bhi kar denge or like or comments bhi kar denge"
+echo "vijesh: thanks dosto for yours supports"
+echo "backbanchers: like or comments maangne ka trika thoda casual hai "
